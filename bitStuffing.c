99791ab946e8aa972a6235e7a08508606fe26d24
AktarSoyeb <aktarsoyeb11@gmail.com>
2025-08-13 12:04:33 +0530

    Second code

---
+++ bitStuffing.c
@@ -0,0 +1,39 @@
+//Bit stuffing
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char bit[50], stuff[50];
+    int count=0,i=0,j=0;
+
+    printf("Enter the bit sequence: \n");
+    scanf("%s",bit);
+
+    for(i=0;i<strlen(bit);i++){
+        if(bit[i]=='0')
+            count=0;
+        else if(bit[i]=='1')
+            count++;
+        stuff[j++]=bit[i];
+        
+        if(count==5){
+            stuff[j++]='0';
+            count=0;
+        }
+
+}
+
+stuff[j]='\0';
+
+printf("Bit array:");
+printf("%s \r\n ",bit);
+
+printf("Stuffed array: ");
+printf("%s\r\n ",stuff);
+
+
+return 0;
+
+}
+
